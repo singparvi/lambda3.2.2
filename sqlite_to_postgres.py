9e5234dd44e85a39751cbc77fe7585db69cb0f53
Parvi <sing.parvi1@gmail.com>
2021-04-08 18:32:10 -0400

    2021-04-08 Commit in 332

--- sqlite_to_postgres.py
+++ sqlite_to_postgres.py
@@ -1,30 +1,44 @@
 import sqlite3
 import pandas as pd
 import psycopg2
+from decouple import config
 
-columns = ['character_id', 'name', 'level', 'exp', 'hp', 'strength', 'intelligence', 'dexterity', 'wisdom']
-conn = sqlite3.connect('rpg_db.sqlite3')
-curs = conn.cursor()
+MY_PASSWORD = config('MY_PASSWORD')
+print(MY_PASSWORD)
 
-pg_conn = psycopg2.connect(dbname='mlrgffyq', user='mlrgffyq', password='O6SgFeUVSIUk1q0cvOhV4IM6vsyrSRmS',
-                           host='queenie.db.elephantsql.com')
-
-pg_curs = pg_conn.cursor()
-
-character_query = '''SELECT * FROM charactercreator_character'''
-
-results = curs.execute(character_query).fetchall()
-
-sqlite_df = pd.DataFrame(results)
-
-sqlite_df.columns = columns
-
-for row in sqlite_df.iterrows():
-    characters_insert_query = """
-INSERT INTO character_sqlite ({})
-VALUES ({})
-""".format(", ".join(columns), "'" + "', '".join([str(x) for x in row[1]]) + "'")
-    pg_curs.execute(characters_insert_query)
-    pg_conn.commit()
-
-conn.close()
+# python-dotenv
+# from dotenv import load_dotenv
+# import os
+# load_dotenv()
+# MY_PASSWORD = os.getenv('MY_PASSWORD')
+#
+# python-decouple
+# from decouple import config
+# MY_PASSWORD = config('MY_PASSWORD')
+#
+# columns = ['character_id', 'name', 'level', 'exp', 'hp', 'strength', 'intelligence', 'dexterity', 'wisdom']
+# conn = sqlite3.connect('rpg_db.sqlite3')
+# curs = conn.cursor()
+#
+# pg_conn = psycopg2.connect(dbname='mlrgffyq', user='mlrgffyq', password='O6SgFeUVSIUk1q0cvOhV4IM6vsyrSRmS',
+#                            host='queenie.db.elephantsql.com')
+#
+# pg_curs = pg_conn.cursor()
+#
+# character_query = '''SELECT * FROM charactercreator_character'''
+#
+# results = curs.execute(character_query).fetchall()
+#
+# sqlite_df = pd.DataFrame(results)
+#
+# sqlite_df.columns = columns
+#
+# for row in sqlite_df.iterrows():
+#     characters_insert_query = """
+# INSERT INTO character_sqlite ({})
+# VALUES ({})
+# """.format(", ".join(columns), "'" + "', '".join([str(x) for x in row[1]]) + "'")
+#     pg_curs.execute(characters_insert_query)
+#     pg_conn.commit()
+#
+# conn.close()
